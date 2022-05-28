99c7193a8d3392d54fa4d617d6f527482bef6f3d
nely de faria <nelly.nf54@gmail.com>
2022-05-28 18:22:56 -0300

    Criando objeto

--- frontend_essentials/section_03/script-variaves.js
+++ frontend_essentials/section_03/script-variaves.js
@@ -1,3 +1,14 @@
 var firstName = 'Luan';
 let role = 'Front-end developer';
 const codigo = 1112;
+
+const human = {
+    name: 'Luan Monteiro',
+    age: 30,
+    weight: 90,
+    isAthlete: true
+}
+
+console.log(human);
+console.log(human.name);
+console.log(human.age);
