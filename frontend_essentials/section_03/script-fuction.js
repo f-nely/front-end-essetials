d10d5ce023ca52d2868add9ec0047c8f8347b8be
nely de faria <nelly.nf54@gmail.com>
2022-05-28 18:25:27 -0300

    Criando arquivo script-fuction.js

---
+++ frontend_essentials/section_03/script-fuction.js
@@ -0,0 +1,11 @@
+function myFirstFunction() {
+    console.log('Olhaaaa a função!!');
+}
+
+myFirstFunction();
+
+function sum(number1, number2) {
+    return number1 + number2;
+}
+
+console.log(sum(32, 24));
