6520b088a88d53c9c6784b6666e20bf360478bef
nely de faria <nelly.nf54@gmail.com>
2022-05-28 18:38:18 -0300

    Altera título + cria função anônima

--- frontend_essentials/section_03/script-fuction.js
+++ frontend_essentials/section_03/script-fuction.js
@@ -16,3 +16,12 @@ const multiplication = (n1, n2) => {
 }
 
 console.log(multiplication(11, 6));
+
+// Função anônima
+(function () {
+    console.log('Paylivre S2');
+})();
+
+setTimeout(function() {
+    console.log('Vou levar 3 segundo para ser executado!');
+}, 3000);
