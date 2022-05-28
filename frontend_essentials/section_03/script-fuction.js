2c74ad09da52d5f1ecafc75076e7fa4a53952dbc
nely de faria <nelly.nf54@gmail.com>
2022-05-28 18:31:17 -0300

    Criando arrow function

--- frontend_essentials/section_03/script-fuction.js
+++ frontend_essentials/section_03/script-fuction.js
@@ -9,3 +9,10 @@ function sum(number1, number2) {
 }
 
 console.log(sum(32, 24));
+
+// Arrow function
+const multiplication = (n1, n2) => {
+    return n1 * n2;
+}
+
+console.log(multiplication(11, 6));
