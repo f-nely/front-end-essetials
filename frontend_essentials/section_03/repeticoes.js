c2ae954a5fb9695412fff2c0ed82e199312a8c22
nely de faria <nelly.nf54@gmail.com>
2022-05-29 20:46:31 -0300

    Criando arquivo repeticoes.js

---
+++ frontend_essentials/section_03/repeticoes.js
@@ -0,0 +1,20 @@
+// for of
+let number = [2, 4, 3, 5, 6];
+
+// for of (valores do array)
+for (num of number) {
+    console.log(num);
+}
+
+console.log('');
+
+// for in
+let animals = {
+    name: 'lion',
+    wight: 200,
+    age: 4
+}
+
+for (item in animals) {
+    console.log(animals[item]);
+}
